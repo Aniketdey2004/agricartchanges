288f100f7e8a804d7852edae7a108adfb0d4c027
[soumashree das] <[soumshreedas2004@gmail.com]>
2024-09-10 18:59:17 +0530

    subscripotion routes specified via comments

--- BACKEND/src/routes/subscriptionCSA.routes.js
+++ BACKEND/src/routes/subscriptionCSA.routes.js
@@ -5,18 +5,18 @@ const router = Router();
 
 // Route to add a new subscription by farmer
 router.post('/addSubscription', subscriptionNew);
-//http://localhost:3026/api/v1/CSA/addSubscription
+//http://localhost:3026/api/v1/subscriptions/addSubscription
 
 // Route to get all subscriptions by under a farmer
-router.post('/:farmerId', getAllPlansByFarmerId);
-//http://localhost:3026/api/v1/CSA/addSubscription
+router.get('/:farmerId', getAllPlansByFarmerId);
+//http://localhost:3026/api/v1/subscriptions/:farmerId
 
 // Route to get all subscriptions by under a farmer
 router.post('/', getAllSubscriptions);
-//http://localhost:3026/api/v1/CSA/addSubscription
+//http://localhost:3026/api/v1/subscriptions/
 
 // // Route to get all subscriptions by under a farmer
 // router.post('/:farmerId', getAllPlansByFarmerId);
-// //http://localhost:3026/api/v1/CSA/addSubscription
+// //http://localhost:3026/api/v1/subscriptions/addSubscription
 
 export default router;
