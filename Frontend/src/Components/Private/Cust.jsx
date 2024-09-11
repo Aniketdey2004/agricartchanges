e11665474fdeec7e1778c6a5779f8c0bd22d804d
Aniket Dey <deyaniket48@gmail.com>
2024-09-11 19:40:10 +0530

    routing protection updated

--- Frontend/src/Components/Private/Cust.jsx
+++ Frontend/src/Components/Private/Cust.jsx
@@ -1,8 +1,8 @@
 import { UserContext } from "../../contexts/UserContext";
 import { useContext } from "react";
-
+import { Navigate } from "react-router-dom";
 export default function Cust({ component: Component }) {
     const { loggedUser } = useContext(UserContext);
 
-    return loggedUser?.loggedInUser ? <Component /> : null;
+    return loggedUser?.loggedInUser ? <Component /> : <Navigate to="/login"/>;
 }
--- Frontend/src/Components/Private/Farmer.jsx
+++ Frontend/src/Components/Private/Farmer.jsx
@@ -1,8 +1,9 @@
 import { UserContext } from "../../contexts/UserContext";
 import { useContext } from "react";
+import { Navigate } from "react-router-dom";
 
 export default function Farmer({ component: Component }) {
     const { loggedUser } = useContext(UserContext);
 
-    return loggedUser?.loggedInFarmer ? <Component /> : null;
+    return loggedUser?.loggedInFarmer ? <Component /> : <Navigate to="/login-farmer"/>;
 }
